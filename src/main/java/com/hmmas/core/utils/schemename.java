9e86600fb45249789746d407fcb3f699c2789b41
emagodi <magodiedwin@gmail.com>
2022-11-02 17:37:40 +0200

    Add schemename enum

---
+++ src/main/java/com/hmmas/core/utils/schemename.java
@@ -0,0 +1,14 @@
+package com.hmmas.core.utils;
+
+public enum schemename {
+
+    Basic,
+    Primary,
+    Premium,
+    Executive,
+    Extended_basic,
+    Extended_primary,
+    Extended_Premium,
+    Extended_Executive,
+    Pensioners
+}
