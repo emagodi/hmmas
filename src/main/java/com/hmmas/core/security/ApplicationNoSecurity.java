be9036a5e1e7278cc23550c2b1e233ad012770e3
emagodi <magodiedwin@gmail.com>
2022-11-02 19:40:57 +0200

    Removed security for dev profile

---
+++ src/main/java/com/hmmas/core/security/ApplicationNoSecurity.java
@@ -0,0 +1,17 @@
+package com.hmmas.core.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+@Profile("dev")
+public class ApplicationNoSecurity {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .antMatchers("/**");
+    }
+}
