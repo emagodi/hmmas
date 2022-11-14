d13668a2153a69fb3addec3e988ef15e2111fca3
emagodi <magodiedwin@gmail.com>
2022-11-14 09:58:44 +0200

    Add membertype enum

---
+++ src/main/java/com/hmmas/core/utils/MemberType.java
@@ -0,0 +1,14 @@
+package com.hmmas.core.utils;
+
+public enum MemberType {
+    ADULT_MEMEBR,
+    ADULT_DEPENDENT,
+    CHILD,
+    SPECIAL_ADULT,
+    SPECIAL_CHILD,
+    PENSIONER,
+    Special_Principal,
+    Pensioner_EX_Employe,
+    Executive_Csh_Spouse,
+    Spouse
+}
